10e96f0498ab03cb55e13ef9b175814dcb5169bf
Shekha Desai <shekha.desai@beyondata.co.in>
2020-07-21 21:20:45 +0530

    Updates

---
+++ precache-manifest.f59f07fe9d74e6153ab40e11d51f5d93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f047e83ea874e0bdb4e7f7e665ea885",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "3f6c2e1b07bbbaac8b97",
+    "url": "/react-resume/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a17bf967ac26a8d3feeb",
+    "url": "/react-resume/static/js/2.5f887cf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-resume/static/js/2.5f887cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6c2e1b07bbbaac8b97",
+    "url": "/react-resume/static/js/main.8389b74f.chunk.js"
+  },
+  {
+    "revision": "6e3dcb157e6085e9a613",
+    "url": "/react-resume/static/js/runtime-main.c7d6af18.js"
+  }
+]);
