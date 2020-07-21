c6539320306639fb9e6ae14450666874b97fbcc1
Shekha Desai <shekha.desai@beyondata.co.in>
2020-07-21 21:17:44 +0530

    try

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Header from './components/header/header';
 import About from './components/about/about';
 import Resume from './components/resume/resume';
-import Portfolio from './components/portfolio/portfolio';
 import Testimonials from  './components/testimonials/testimonials';
 import Footer from './components/footer/footer';
 
